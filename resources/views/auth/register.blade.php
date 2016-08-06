0a90a9968888012c9056b89733de0c11806208a4
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-08-07 00:53:48 +0900

    change register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,6 @@
 @section('meta')
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>ファンクリターン</title>
-<meta name="description" content="{{ $data['discription'] }}"/>
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
