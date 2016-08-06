d7b37a938bca428a7e517516987db9d6fc70af0d
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-08-07 04:14:00 +0900

    change the main title and the icon

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -13,7 +13,7 @@
 @section('content')
 @include('elements/pc/base/header', ['bgcolor' => 'ruby'])
 @include('elements/pc/base/pankuzu', ['pankuzu' => ['Home' => 'disable']])
-@include('elements/pc/base/title', ['title' => 'Keijiの技術ブログ <small>tech</small><span class="glyphicon glyphicon-user right color-777"></span>'])
+@include('elements/pc/base/title', ['title' => 'FuncReturn<span class="glyphicon glyphicon-equalizer right color-444"></span>'])
 @include('elements/pc/article/index/contents')
 @include('elements/pc/base/footer', ['bgcolor' => 'ruby'])
 @endsection
