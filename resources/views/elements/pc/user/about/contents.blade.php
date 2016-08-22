d5d6d495a4c61934fe216a0bd3fc9e073bba0999
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-08-22 19:39:54 +0900

    remove github follow button

--- resources/views/elements/pc/user/about/contents.blade.php
+++ resources/views/elements/pc/user/about/contents.blade.php
@@ -41,7 +41,7 @@
 				  </div>
 			  </div>
 			</div>
-		  <div class=" col-xs-12 col-md-6 right">
+		  <!-- <div class=" col-xs-12 col-md-6 right">
 				<div class="panel panel-default">
 				  <div class="panel-heading">
 				    <h3 class="panel-title">Github</h3>
@@ -50,27 +50,7 @@
 				  	<a class="github-button" href="https://github.com/keijinamba" data-style="mega" data-count-href="/keijinamba/followers" data-count-api="/users/keijinamba#followers">Follow @keijinamba</a>
 				  </div>
 			  </div>
-		  </div>
+		  </div> -->
 		</div>
-		<!-- <div class="panel panel-default user-about">
-		  <div class="panel-heading">
-		    <h3 class="panel-title">Follow Me</h3>
-		  </div>
-		  <div class="panel-body">
-		  	<div class="col-xs-12 col-md-6 sns-box">
-		  		<div class="well">
-		        <a class="twitter-timeline" data-height="500" data-theme="dark" href="https://twitter.com/keiji_1110">Tweets by keiji_1110</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-		      </div>
-		    </div>
-		    <div class="col-xs-12 col-md-6 sns-box">
-		    	<div class="well">
-			   	  <div class="fb-follow" data-href="https://www.facebook.com/keiji.namba" data-height="300" data-layout="standard" data-size="large" data-show-faces="true"></div>
-			   	</div>
-			   	<div class="well">
-			      <a class="github-button" href="https://github.com/keijinamba" data-style="mega" data-count-href="/keijinamba/followers" data-count-api="/users/keijinamba#followers">Follow @keijinamba</a>
-			    </div>
-		    </div>
-		  </div>
-		</div> -->
   </div>
 </div>
