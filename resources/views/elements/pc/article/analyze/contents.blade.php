50649eb9b46b9ea9a38de317647f4d0d083f3c8b
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-09-21 23:25:26 +0900

    not finished

---
+++ resources/views/elements/pc/article/analyze/contents.blade.php
@@ -0,0 +1,11 @@
+<div class="search-main col-sm-12 col-md-12">
+	<div class="w-auto">
+		<div class="jumbotron  col-sm-12 col-md-12">
+			<div class="container">
+		    <h1>形態素解析</h1>
+		    <p>形態素解析エンジンMeCabを使って文章を単語レベルで分割し、自然言語処理の入り口に立つ。<br>MeCabの導入方法を紹介した記事は以下のボタンから飛べます。</p>
+ 				<p><a class="btn btn-primary btn-lg" href="#" role="button">MeCabの導入</a></p>
+		  </div>
+		</div>
+	</div>
+</div>
