e014c64cfe074981e6be50afc49612a009eb0b8e
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-09-23 00:32:03 +0900

    update the design of the top page

--- resources/views/elements/pc/article/analyze/contents.blade.php
+++ resources/views/elements/pc/article/analyze/contents.blade.php
@@ -4,7 +4,7 @@
 			<div class="container">
 		    <h1>形態素解析</h1>
 		    <p>形態素解析エンジンMeCabを使って文章を単語レベルで分割し、自然言語処理の入り口に立つ。<br>MeCabの導入方法を紹介した記事は以下のボタンから飛べます。</p>
- 				<p><a class="btn btn-primary btn-lg" href="/16" role="button">MeCabの導入</a></p>
+ 				<p><a class="btn btn-primary btn-lg shadow2" href="/16" role="button">MeCabの導入</a></p>
 		  </div>
 		</div>
 	</div>
@@ -13,7 +13,7 @@
 <div class="jumbotron col-sm-12 col-md-12 analyze-main">
 	<div class="container">
 		<div class="bs-callout bs-callout-purple" id="callout-glyphicons-location">
-			<h4>形態素解析を体験</h4>
+			<h4>形態素解析を試す</h4>
 			<p>形態素解析とは、自然言語のテキストデータ（文）から、対象言語の文法や、辞書と呼ばれる単語の品詞等の情報にもとづき、言語で意味を持つ最小単位の列に分割し、それぞれの形態素の品詞等を判別すること。<br><a href="https://ja.wikipedia.org/wiki/%E5%BD%A2%E6%85%8B%E7%B4%A0%E8%A7%A3%E6%9E%90">ー Wikipedia </a></p>
 			<ul>
 				<li>形態素解析エンジンとしてMeCabを使っています。</li>
--- resources/views/elements/pc/article/index/contents.blade.php
+++ resources/views/elements/pc/article/index/contents.blade.php
@@ -1,4 +1,3 @@
 <div style="overflow:hidden;">
 @include('elements/pc/article/index/main')
-@include('elements/pc/base/side')
 </div>
