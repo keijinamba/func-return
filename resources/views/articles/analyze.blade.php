f051099af17b9d2cd76efee245701c27e92dca51
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-09-23 16:03:26 +0900

    change title to FuncReturn

--- resources/views/articles/analyze.blade.php
+++ resources/views/articles/analyze.blade.php
@@ -2,7 +2,7 @@
 
 @section('meta')
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>自然言語処理｜ファンクリターン</title>
+<title>自然言語処理｜FuncReturn</title>
 <meta name="description" content="自然言語処理をあれこれとやっていくページです。形態素解析エンジンMeCabを使って与えられた文章を単語レベルで分割して形態素解析がどのようなものか体験できます。"/>
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
